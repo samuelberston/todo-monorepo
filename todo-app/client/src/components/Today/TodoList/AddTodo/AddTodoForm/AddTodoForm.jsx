29b43241fa28825c8ad99662d2083f775e5db961
Samuel Berston <samuelberston@gmail.com>
2022-11-13 15:19:57 -0800

    use handler to update description in state

--- todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
+++ todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
@@ -14,11 +14,19 @@ const AddTodoForm = (props) => {
         }));
     }
 
+    const handleDescriptionInputChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            description: event.target.value
+        }));
+    }
+
     return (
         <div id="addTodoForm">
             <form>
                 <input type="text" name="taskName" placeholder="Task name" value={values.taskName} onChange={handleTaskNameInputChange}></input>
-                <input type="text" name="description" placeholder="Decsription"></input>
+                <input type="text" name="description" placeholder="Decsription" value={values.description} onChange={handleDescriptionInputChange}></input>
             </form>
             <button onClick={props.clickHandler}>
                 Cancel
