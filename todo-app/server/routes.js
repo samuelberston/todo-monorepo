de7d33e6ed35eeaf77df12f78fc561e5a830a59b
Samuel Berston <samuelberston@gmail.com>
2022-11-13 14:41:11 -0800

    create /todo post route

--- todo-app/server/routes.js
+++ todo-app/server/routes.js
@@ -14,11 +14,19 @@ router.get('/todos', (req, res) => {
 // create a new todo item
 router.post('/todos', (req, res) => {
   // insert new todo into db
+  const { task, description, date_created, date_due, priority } = req.body;
+
+  db.query(
+    `INSERT INTO todos (task, description, date_created, date_due, priority) 
+    VALUES ("${task}", "${description}", "${date_created}", "${date_due}", "${priority}")`,
+    (err, data) => {
+      if (err) { throw err; }
+      res.sendStatus(201).send();
+    });
 });
 
 // delete a todo item
 router.delete('/todos', (req, res) => {
-  console.log('delete');
   const todoId = req.query.todoId;
   // remove from db
   db.query(`DELETE FROM todos WHERE todo_id = ${todoId}`, (err, data) => {
