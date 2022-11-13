3eb6c0c7c86b0e2cd4b747655783749ff66b1823
Samuel Berston <samuelberston@Samuels-MacBook-Air.local>
2022-11-13 00:29:26 -0800

    create database schema

--- todo-app/schema.sql
+++ todo-app/schema.sql
@@ -1,30 +1,37 @@
-DROP DATABASE IF EXISTS MVP;
+DROP DATABASE IF EXISTS TODO;
 
-CREATE DATABASE MVP;
+CREATE DATABASE TODO;
 
-USE MVP;
+USE TODO;
 
-CREATE TABLE msgs (
-  message_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(50) NOT NULL,
-  receiver VARCHAR(14) NOT NULL,
-  text VARCHAR(255) NOT NULL,
-  cron VARCHAR(100) NOT NULL
+CREATE TABLE todos (
+  todo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  task VARCHAR(50) NOT NULL,
+  description VARCHAR(14),
+  date_created VARCHAR(24) NOT NULL,
+  date_due VARCHAR(24) NOT NULL,
+  priority VARCHAR(2)
 );
 
-INSERT INTO msgs (message_id, title, receiver, text, cron) VALUES (1, "trash", "+14151234567", "Remember to take out the trash!", "30 1 Monday * August");
-INSERT INTO msgs (message_id, title, receiver, text, cron) VALUES (2, "good morning", "+14151234567", "good morning beautiful !", "0 24 * 3 September");
-INSERT INTO msgs (message_id, title, receiver, text, cron) VALUES (3, "Rhonda", "+14151234567", "Did you remember to water the plants??", "45 16 Tuesday, Wednesday * *");
-INSERT INTO msgs (message_id, title, receiver, text, cron) VALUES (4, "reminder", "+14151234567", "buy tickets to edc", "* * * * *");
+INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (1, "Create a todo list app with Node.js", "", "2022-11-13T00:46:19.750Z", "", "P2");
+INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (2, "Publish the app on GitHub", "", "2022-11-13T00:48:59.204Z", "", "P1");
+INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (3, "Deploy the app on aws", "", "2022-11-13T00:55:15.320Z", "", "P2");
 
-CREATE TABLE contacts (
-  contact_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  firstName VARCHAR(25) NOT NULL,
-  lastName VARCHAR(25) NOT NULL,
-  number VARCHAR(12) NOT NULL,
-  email VARCHAR(50) NOT NULL
+CREATE TABLE tags (
+  tag_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag VARCHAR(25) NOT NULL
 );
 
-INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (1, "Sam", "Berston", "+14158468793", "samuelberston@gmail.com");
-INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (2, "Mariah", "Carey", "+12129276505", "mariahcarey@gmail.com");
-INSERT INTO contacts (contact_id, firstName, lastName, number, email) VALUES (3, "Cardi", "B", "+12128082233", "BelcalisAlmánzar@gmail.com");
+INSERT INTO tags (tag_id, tag) VALUES (1, "Work");
+INSERT INTO tags (tag_id, tag) VALUES (2, "Personal");
+INSERT INTO tags (tag_id, tag) VALUES (3, "Social");
+
+CREATE TABLE todos_tags (
+  todo_id INT NOT NULL,
+  tag_id INT NOT NULL
+);
+
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (1, 1);
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (2, 1);
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 2);
+
