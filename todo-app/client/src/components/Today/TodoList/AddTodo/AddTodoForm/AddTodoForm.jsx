a4c5bf7c016f541cb66ae2dc01d6257d968da555
Samuel Berston <samuelberston@gmail.com>
2022-11-13 15:18:37 -0800

    use handler to update taskName in state

--- todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
+++ todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AddTodoForm = (props) => {
+    const [values, setValues] = useState({
+        taskName: '',
+        description: ''
+    });
+
+    const handleTaskNameInputChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            taskName: event.target.value
+        }));
+    }
+
     return (
         <div id="addTodoForm">
-            addTodoForm
+            <form>
+                <input type="text" name="taskName" placeholder="Task name" value={values.taskName} onChange={handleTaskNameInputChange}></input>
+                <input type="text" name="description" placeholder="Decsription"></input>
+            </form>
             <button onClick={props.clickHandler}>
                 Cancel
             </button>
