2a0f83f9f57aa32e12aafca18c83d199cc62abdc
Samuel Berston <samuelberston@gmail.com>
2022-11-13 15:37:43 -0800

    create handleSubmit function

--- todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
+++ todo-app/client/src/components/Today/TodoList/AddTodo/AddTodoForm/AddTodoForm.jsx
@@ -5,6 +5,16 @@ const AddTodoForm = (props) => {
         taskName: '',
         description: ''
     });
+    const [errors, setErrors] = useState({})
+
+    const handleValidation = (event) => {
+        let valid = true;
+        if (values.taskName == '' || values.taskName == null) {
+            valid = false;
+            setErrors(errors["taskName"] = "Cannot be empty");
+        }
+        return valid;
+    }
 
     const handleTaskNameInputChange = (event) => {
         event.persist();
@@ -22,15 +32,24 @@ const AddTodoForm = (props) => {
         }));
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (handleValidation(event)) {
+            // send post request to /todos API endpoint with request body
+            // reset form
+        } else {
+            alert('form contains errors')
+        }
+    }
+
     return (
-        <div id="addTodoForm">
+        <div id="addTodoForm" onSubmit={handleSubmit}>
             <form>
                 <input type="text" name="taskName" placeholder="Task name" value={values.taskName} onChange={handleTaskNameInputChange}></input>
-                <input type="text" name="description" placeholder="Decsription" value={values.description} onChange={handleDescriptionInputChange}></input>
+                <input type="text" name="description" placeholder="Description" value={values.description} onChange={handleDescriptionInputChange}></input>
+                <button onClick={props.clickHandler}> Cancel </button>
+                <button type="submit"> Add Task </button>
             </form>
-            <button onClick={props.clickHandler}>
-                Cancel
-            </button>
         </div>
     );
 }
