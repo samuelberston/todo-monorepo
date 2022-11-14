a0452dc6c5747d2d2893944ee881a7678c4986f2
Samuel Berston <samuelberston@gmail.com>
2022-11-13 16:47:21 -0800

    refactor routes to TodosRouter

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const dotenv = require('dotenv').config()
 
 const express = require('express');
-const router = require('./routes/routes.js');
+const TodosRouter = require('./routes/TodosRouter.js');
 
 const app = express();
 const port = 3000;
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', router);
+app.use('/', TodosRouter);
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
