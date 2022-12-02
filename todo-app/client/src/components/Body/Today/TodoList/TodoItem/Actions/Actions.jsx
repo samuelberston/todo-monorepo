f0cfaff1f19e672c2e2cedee6b1bd04300b83cfd
Samuel Berston <samuelberston@gmail.com>
2022-12-02 10:09:41 -0800

    Actions styling -- need to factor out actions css from TodoItem css

--- todo-app/client/src/components/Body/Today/TodoList/TodoItem/Actions/Actions.jsx
+++ todo-app/client/src/components/Body/Today/TodoList/TodoItem/Actions/Actions.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import styles from '../TodoItem.module.css';
 
+import UpdateTodo from './UpdateTodo/UpdateTodo.jsx';
+
 const Actions = () => {
     return (
         <div id={styles.actions} >
+            <i class="fas fa-edit"></i>
             <i class="fa-solid fa-ellipsis"></i>
         </div>
     );
