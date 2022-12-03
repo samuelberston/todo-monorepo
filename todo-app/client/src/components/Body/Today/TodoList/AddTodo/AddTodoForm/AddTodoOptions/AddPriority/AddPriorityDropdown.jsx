8a13ee85d2eb9567bb4d530a8c8b69490bddd6dc
Samuel Berston <samuelberston@gmail.com>
2022-12-02 22:28:40 -0800

    AddPriorityDropdown updates AddTodoForm state

--- todo-app/client/src/components/Body/Today/TodoList/AddTodo/AddTodoForm/AddTodoOptions/AddPriority/AddPriorityDropdown.jsx
+++ todo-app/client/src/components/Body/Today/TodoList/AddTodo/AddTodoForm/AddTodoOptions/AddPriority/AddPriorityDropdown.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 
 const AddPriorityDropdown = (props) => {
-    const [selectValue, setSelectValue] = useState("p4");
-
     return (
-        <select id="priorityDropdown">
+        <select id="priorityDropdown" onChange={props.handlePriorityInputChange}>
             <option value="p1">Priority 1</option>
             <option value="p2">Priority 2</option>
             <option value="p3">Priority 3</option>
