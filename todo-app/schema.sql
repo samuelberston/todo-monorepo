739ff3dcfe5df5fbbd14dace22ffa4faeba4ec91
Samuel Berston <samuelberston@gmail.com>
2022-12-11 16:07:12 -0800

    update schema.sql

--- todo-app/schema.sql
+++ todo-app/schema.sql
@@ -7,7 +7,7 @@ USE TODO;
 CREATE TABLE todos (
   todo_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   task VARCHAR(50) NOT NULL,
-  description VARCHAR(65535),
+  description VARCHAR(1000),
   date_created VARCHAR(24) NOT NULL,
   date_due VARCHAR(24) NOT NULL,
   priority VARCHAR(2)
@@ -35,5 +35,7 @@ CREATE TABLE todos_tags (
 
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (1, 1);
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (2, 1);
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 1);
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 2);
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 3);
 
