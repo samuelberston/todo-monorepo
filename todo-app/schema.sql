0f48793a5d007934d123df5bdb821e275deba789
Samuel Berston <samuelberston@gmail.com>
2022-12-11 16:08:36 -0800

    update schema.sql with new todo

--- todo-app/schema.sql
+++ todo-app/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE todos (
 INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (1, "Create a todo list app with Node.js", "", "2022-11-13T00:46:19.750Z", "", "p2");
 INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (2, "Publish the app on GitHub", "", "2022-11-13T00:48:59.204Z", "", "p1");
 INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (3, "Deploy the app on aws", "", "2022-11-13T00:55:15.320Z", "", "p3");
+INSERT INTO todos (todo_id, task, description, date_created, date_due, priority) VALUES (4, "Containerize the app with Docker", "", "2022-11-13T00:55:15.320Z", "", "p4");
 
 CREATE TABLE tags (
   tag_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -25,6 +26,7 @@ CREATE TABLE tags (
 INSERT INTO tags (tag_id, tag) VALUES (1, "Work");
 INSERT INTO tags (tag_id, tag) VALUES (2, "Personal");
 INSERT INTO tags (tag_id, tag) VALUES (3, "Social");
+INSERT INTO tags (tag_id, tag) VALUES (4, "Backlog");
 
 CREATE TABLE todos_tags (
   todo_id INT NOT NULL,
@@ -38,4 +40,5 @@ INSERT INTO todos_tags (todo_id, tag_id) VALUES (2, 1);
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 1);
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 2);
 INSERT INTO todos_tags (todo_id, tag_id) VALUES (3, 3);
+INSERT INTO todos_tags (todo_id, tag_id) VALUES (4, 4);
 
