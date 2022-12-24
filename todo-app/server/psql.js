dcc4c66f758fcfbf77d57ab285ec6377bc09848f
sberston <sberston@salesforce.com>
2022-12-23 19:29:44 -0600

    psql.js

---
+++ todo-app/server/psql.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg')
+
+const user = 'sberston';
+const host = 'localhost';
+const port = 5432;
+const database = 'todo';
+
+const postgres = new Client({
+    host,
+    port,
+    user,
+    database
+});
+
+postgres.connect((err) => {
+    if (err) { throw err; }
+    console.log(`database connected at ${port}`);
+});
+
+module.exports = postgres;
