b9b65fee0d7365309ea89333ba9a5f4b6db98a50
sberston <sberston@salesforce.com>
2022-12-29 16:35:02 -0600

    comments for future work in server.js

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -13,6 +13,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+// refactor this to run the routes in a separate server, so I can isolate the UI
+// then, connect to that server ... and use that as middleware
 app.use('/', TodosRouterPsql);
 app.use('/', TagsRouterPsql);
 
