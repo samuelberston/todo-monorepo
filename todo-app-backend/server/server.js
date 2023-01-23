31cdb9fc36e7511f7fc5a634f04dfd29203964ef
Samuel Berston <samuelberston@gmail.com>
2023-01-22 17:58:25 -0800

    cors

--- todo-app-backend/server/server.js
+++ todo-app-backend/server/server.js
@@ -11,6 +11,12 @@ const port = 3000;
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // refactor this to run the routes in a separate server, so I can isolate the UI
 // then, connect to that server ... and use that as middleware
 app.use('/', TodosRouterPsql);
