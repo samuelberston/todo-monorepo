f1b3dab669fba2226358fc99c8787384b60441e4
Samuel Berston <samuelberston@gmail.com>
2023-01-23 14:47:49 -0800

    cores

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -6,9 +6,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8443");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
