2ddd7ab70ce4b281c017ec74bbf403474cea4009
Samuel Berston <samuelberston@gmail.com>
2023-02-12 17:12:59 -0800

    auth_config,json, install auth0-react SDK, refactor to use createBrowserRouter

--- todo-app/client/src/index.js
+++ todo-app/client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './components/App/App.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <App />
+    ),
+  },
+]);
+
 render((
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 ), document.getElementById('root'));
