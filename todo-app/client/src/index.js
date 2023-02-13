f37476bf23fdb3e9cad77995c159e4cfbc2dbd40
Samuel Berston <samuelberston@gmail.com>
2023-02-12 17:00:20 -0800

    refactor react-router-dom to use BrowserRouter with Routes/Route

--- todo-app/client/src/index.js
+++ todo-app/client/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './components/App/App.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
-
 render((
     <React.StrictMode>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>
 ), document.getElementById('root'));
