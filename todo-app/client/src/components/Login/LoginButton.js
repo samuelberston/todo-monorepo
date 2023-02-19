bbc309e047debb1115ff16cecd7b905966e6d479
Samuel Berston <samuelberston@gmail.com>
2023-02-19 14:11:54 -0800

    LoginButton.js

---
+++ todo-app/client/src/components/Login/LoginButton.js
@@ -0,0 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/profile',
+      },
+    });
+  };
+
+  return (
+    <button className="button__login" onClick={handleLogin} type={}>
+      Log In
+    </button>
+  );
+};
+
+export default LoginButton;
