67a3e7fe6155fcd2464a88591293f1dbfce6cf46
sberston <sberston@salesforce.com>
2023-02-23 14:09:14 -0800

    cors

--- todo-app-backend/server/server.js
+++ todo-app-backend/server/server.js
@@ -1,23 +1,26 @@
+const cors = require("cors");
 const path = require('path');
 const dotenv = require('dotenv').config()
 const express = require('express');
 
-console.log(process.env);
-
 const TodosRouterPsql = require('./routes/TodosRouterPsql.js');
 const TagsRouterPsql = require('./routes/TagsRouterPsql.js')
 
-const app = express();
 const PORT = parseInt(process.env.PORT, 10);
+const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
+
+const app = express();
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN_URL,
+    methods: ["GET, PUT, POST, DELETE"],
+    allowedHeaders: ["Authorization, Origin, X-Requested-With, Content-Type, Accept"],
+    maxAge: 86400,
+  })
+);
 
 // refactor this to run the routes in a separate server, so I can isolate the UI
 // then, connect to that server ... and use that as middleware
