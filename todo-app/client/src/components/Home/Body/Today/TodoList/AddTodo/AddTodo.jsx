cdfc3c834fe46cca0f0b059adb3951ffc1751765
sberston <sberston@salesforce.com>
2023-02-22 16:25:11 -0800

    refactor lazy loading in AddTodo for React 18

--- todo-app/client/src/components/Home/Body/Today/TodoList/AddTodo/AddTodo.jsx
+++ todo-app/client/src/components/Home/Body/Today/TodoList/AddTodo/AddTodo.jsx
@@ -18,9 +18,9 @@ const AddTodo = (props) => {
         <div id="addTodoContainer" >
             {
                 active ?
-                <Suspense fallback={<div>Loading...</div>}>
+                <React.Suspense fallback={<div>Loading...</div>}>
                     <AddTodoForm mode={"ADD"} clickHandler={clickHandler} loadTodos={props.loadTodos} handleSubmit={AddTodoSubmit} submitText={"Add Task"}/>
-                </Suspense>
+                </ React.Suspense>
                 : <AddTask clickHandler={clickHandler} />
             }
         </div>
