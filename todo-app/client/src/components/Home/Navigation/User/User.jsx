471292b2ee02e78c1f57c2cd401636ecaee8fcfc
sberston <sberston@salesforce.com>
2023-02-23 14:45:16 -0800

    useNagivate hook to redirect on User click and style User element

--- todo-app/client/src/components/Home/Navigation/User/User.jsx
+++ todo-app/client/src/components/Home/Navigation/User/User.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './User.module.css';
 
 const User = (props) => {
     const firstInitial = props.username[0];
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/profile');
+    }
+
     return (
-        <div id="user">
+        <div id="user" className={styles.user} onClick={handleClick}>
             {firstInitial}
         </div>
     );
