200755505bf774052c2dffeac87ce44f5020be6a
sberston <sberston@salesforce.com>
2023-02-22 21:09:31 -0800

    style App.jsx, update localhost to 127.0.0.1 for axios base url

--- todo-app/client/src/components/App/App.jsx
+++ todo-app/client/src/components/App/App.jsx
@@ -2,20 +2,22 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import styles from './App.module.css';
+
 
 import axios from 'axios';
 
 import Home from '../Home/Home.jsx';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://127.0.0.1:3000';
 
 const App = () => {
   const { isLoading } = useAuth0();
 
     if (isLoading) {
       return (
-        <div className="page-layout">
-          <div>loading...</div>
+        <div className={styles.loading}>
+          <div>Loading...</div>
         </div>
       );
     }
