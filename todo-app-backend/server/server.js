c00daf856a6fa6e863a81e356b3b782cc00a602e
sberston <sberston@salesforce.com>
2023-02-22 21:13:31 -0800

    enable all HTTTP methods from 127.0.0.1:8080

--- todo-app-backend/server/server.js
+++ todo-app-backend/server/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
