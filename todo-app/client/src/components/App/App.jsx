82a9acc2ccdd785519e1708f63f0e04c40ccf9a7
sberston <sberston@salesforce.com>
2023-02-22 17:54:44 -0800

    loading page

--- todo-app/client/src/components/App/App.jsx
+++ todo-app/client/src/components/App/App.jsx
@@ -1,5 +1,7 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 
 import axios from 'axios';
 
@@ -7,11 +9,23 @@ import Home from '../Home/Home.jsx';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/app" element={<Home />} />
-  </Routes>
-);
+const App = () => {
+  const { isLoading } = useAuth0();
+
+    if (isLoading) {
+      return (
+        <div className="page-layout">
+          <div>loading...</div>
+        </div>
+      );
+    }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/app" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
