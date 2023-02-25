95db9392014629b915da263aa7bd7e13039a0d0a
sberston <sberston@salesforce.com>
2023-02-25 15:40:47 -0800

    conditionally render Body component based on user authentication status

--- todo-app/client/src/components/Home/Home.jsx
+++ todo-app/client/src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import Navigation from './Navigation/Navigation.jsx';
 import Body from './Body/Body.jsx';
 import styles from './Home.module.css';
 
-const Home = () => (
-  <div id={styles.container}>
-    <Navigation />
-    <Body />
-  </div>
-);
+const Home = () => {
+  const { isAuthenticated, user } = useAuth0();
+
+  return (
+    <div id={styles.container}>
+      <Navigation />
+      { isAuthenticated
+        ? <Body />
+        : <div>Login to view todos</div>
+      }
+    </div>
+  );
+};
 
 export default Home;
