7c86b6b4d3e7e8811a268254d1e7fd3d591efaad
sberston <sberston@salesforce.com>
2023-03-01 22:42:06 -0800

    add audience to Auth0provider

--- todo-app/client/src/Auth0ProviderWithNavigate.js
+++ todo-app/client/src/Auth0ProviderWithNavigate.js
@@ -11,6 +11,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   const config = getConfig()
 
@@ -18,11 +19,16 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
     navigate('/');
   };
 
+  if (!(domain && clientId && redirectUri && audience)) {
+    return null;
+  }
+
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
+        audience: audience,
         redirect_uri: redirectUri,
       }}
       onRedirectCallback={onRedirectCallback}
