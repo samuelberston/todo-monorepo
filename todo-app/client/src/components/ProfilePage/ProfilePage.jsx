3b04c4fd22ef0d446a675e28c3a95babd529e36b
sberston <sberston@salesforce.com>
2023-03-09 15:11:19 -0800

    use lock api in profile page

--- todo-app/client/src/components/ProfilePage/ProfilePage.jsx
+++ todo-app/client/src/components/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import lock from '../../lock/Auth0lock.js';
 import React from "react";
 
 const ProfilePage = () => {
-  const { user } = useAuth0();
+  const { user, getAccessTokenSilently } = useAuth0();
+
+  const getUserInfo = async () => {
+    const accessToken = await getAccessTokenSilently();
+
+    lock.getUserInfo(accessToken, (error, data) => {
+      if (error) { console.error(error); }
+      if (data) { console.log(data); }
+    });
+  }
+  getUserInfo();
+
 
   if (!user) {
     return null;
--- todo-app/client/src/lock/Auth0Lock.js
+++ todo-app/client/src/lock/Auth0Lock.js
@@ -1,44 +1,8 @@
-var Auth = (function() {
+import Auth0Lock from 'auth0-lock';
 
-  var wm = new WeakMap();
-  var privateStore = {};
-  var lock;
+const lock = new Auth0Lock(
+    process.env.REACT_APP_AUTH0_CLIENT_ID,
+    process.env.REACT_APP_AUTH0_DOMAIN
+);
 
-  function Auth() {
-    this.lock = new Auth0Lock(
-      '<{yourClientId}>',
-      '<{yourDomain}>'
-    );
-    wm.set(privateStore, {
-      appName: "example"
-    });
-  }
-
-  Auth.prototype.getProfile = function() {
-    return wm.get(privateStore).profile;
-  };
-
-  Auth.prototype.authn = function() {
-    // Listening for the authenticated event
-    this.lock.on("authenticated", function(authResult) {
-      // Use the token in authResult to getUserInfo() and save it if necessary
-      this.getUserInfo(authResult.accessToken, function(error, profile) {
-        if (error) {
-          // Handle error
-          return;
-        }
-
-        //we recommend not storing Access Tokens unless absolutely necessary
-        wm.set(privateStore, {
-          accessToken: authResult.accessToken
-        });
-
-        wm.set(privateStore, {
-          profile: profile
-        });
-
-      });
-    });
-  };
-  return Auth;
-}());
+export default lock;
