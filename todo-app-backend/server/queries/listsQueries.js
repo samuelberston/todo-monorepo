0dbd753867255c628604d509542b776ab27df819
sberston <sberston@salesforce.com>
2023-04-08 02:13:05 -0700

    post list

--- todo-app-backend/server/queries/listsQueries.js
+++ todo-app-backend/server/queries/listsQueries.js
@@ -1,5 +1,8 @@
 const getUserLists = `SELECT * FROM todo.lists WHERE user_uuid = $1;`;
 
+const postUserLists = `INSERT INTO todo.lists (list_uuid, name, user_uuid) VALUES ($1, $2, $3);`
+
 module.exports = {
-  getUserLists
+  getUserLists,
+  postUserLists
 }
--- todo-app-backend/server/routes/ListsRouterPsql.js
+++ todo-app-backend/server/routes/ListsRouterPsql.js
@@ -1,14 +1,25 @@
 const ListsRouterPsql = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+
 
 const postgres = require('../psql.js');
 
-const { getUserLists } = require('../queries/listsQueries.js');
+const { getUserLists, postUserLists } = require('../queries/listsQueries.js');
 
 ListsRouterPsql.get('/lists', (req, res) => {
   const user_uuid = req.query.user_uuid;
   postgres.query(getUserLists, [user_uuid], (err, data) => {
     if (err) { throw err; }
     res.status(200).send(data.rows);
+  });
+});
+
+ListsRouterPsql.post('/lists', (req, res) => {
+  const {name, user_uuid} = req.body;
+  const list_uuid = uuidv4();
+  postgres.query(postUserLists, [list_uuid, name, user_uuid], (err, data) => {
+    if (err) { throw err; }
+    res.status(204).send([list_uuid]);
   });
 });
 
