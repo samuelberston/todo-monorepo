f65e4301bfe856dd7898ba218f6a47577d7a3db4
sberston <sberston@salesforce.com>
2023-04-10 19:29:04 -0700

    put and delete /lists services

--- todo-app/client/src/services/lists.service.js
+++ todo-app/client/src/services/lists.service.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import callExternalApi from './external-api.service.js';
 
 // get /lists
+// @param user_uuid
 const getUserLists = async (accessToken, user_uuid) => {
   const config = {
     url: '/lists',
@@ -21,9 +22,10 @@ const getUserLists = async (accessToken, user_uuid) => {
     data: data || null,
     error,
   };
-}
+};
 
 // get /lists
+// @param list_uuid
 const getListName = async (accessToken, list_uuid) => {
   const config = {
     url: '/lists',
@@ -43,9 +45,11 @@ const getListName = async (accessToken, list_uuid) => {
     data: data || null,
     error,
   };
-}
+};
 
 // post /lists
+// @param list_name
+// @param user_uuid
 const postLists = async (accessToken, list_name, user_uuid) => {
   const config = {
     url: '/lists',
@@ -67,10 +71,109 @@ const postLists = async (accessToken, list_name, user_uuid) => {
     data: data || null,
     error,
   };
-}
+};
+
+// put /lists
+// @param list_name
+const updateListName = async (accessToken, list_name) => {
+  const config = {
+      url: '/lists',
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        list_name
+      }
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
+// put /lists
+// @param list_uuid
+const incrementListsTodoCount = async (accessToken, list_uuid) => {
+  const config = {
+      url: '/lists',
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        list_uuid
+      }
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
+// put /lists
+// @param list_uuid
+const decrementListsTodoCount = async (accessToken, list_uuid) => {
+  const config = {
+      url: '/lists',
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        list_uuid,
+        dec: true
+      }
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
+// delete /lists
+// @param list_uuid
+// put /lists
+// @param list_uuid
+const deleteLists = async (accessToken, list_uuid) => {
+  const config = {
+      url: '/lists',
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        list_uuid
+      }
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
 
 export {
   getUserLists,
   getListName,
-  postLists
+  postLists,
+  updateListName,
+  incrementListsTodoCount,
+  decrementListsTodoCount,
+  deleteLists
 };
