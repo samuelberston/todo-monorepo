708cbf976806c06a64be2ab456f88db5259a26e7
sberston <sberston@salesforce.com>
2023-04-10 19:23:06 -0700

    put and delete /lists

--- todo-app-backend/server/queries/listsQueries.js
+++ todo-app-backend/server/queries/listsQueries.js
@@ -2,10 +2,28 @@ const getUserLists = `SELECT * FROM todo.lists WHERE user_uuid = $1;`;
 
 const getTodoList = `SELECT * from todo.lists WHERE list_uuid = $1`;
 
-const postUserLists = `INSERT INTO todo.lists (list_uuid, list_name, user_uuid, todo_count) VALUES ($1, $2, $3, 0);`
+const postUserLists = `INSERT INTO todo.lists (list_uuid, list_name, user_uuid, todo_count)
+                       VALUES ($1, $2, $3, 0);`
+
+const putListsName = `UPDATE todo.lists SET todo.lists.list_name = $1
+                     WHERE todo.lists.list_uuid = $2;`
+
+const putListsTodoCount = `UPDATE todo.lists
+                       SET todo.lists.todo_count = todo.lists.todo_count + 1
+                       WHERE todo.lists.list_uuid = $1;`
+
+const decListsTodoCount = `UPDATE todo.lists
+                           SET todo.lists.todo_count = todo.lists.todo_count - 1
+                           WHERE todo.lists.list_uuid = $1;`
+
+const deleteLists = `DELETE FROM todo.lists WHERE todo.lists.list_uuid = $1;`
 
 module.exports = {
   getUserLists,
   getTodoList,
-  postUserLists
+  postUserLists,
+  putListsName,
+  putListsTodoCount,
+  decListsTodoCount,
+  deleteLists
 }
--- todo-app-backend/server/routes/ListsRouterPsql.js
+++ todo-app-backend/server/routes/ListsRouterPsql.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const postgres = require('../psql.js');
 
-const { getUserLists, getTodoList, postUserLists } = require('../queries/listsQueries.js');
+const { getUserLists, getTodoList, postUserLists, putListsName, putListsTodoCount, decListsTodoCount, deleteLists } = require('../queries/listsQueries.js');
 
 ListsRouterPsql.get('/lists', (req, res) => {
   console.log('get /lists', req.query);
@@ -29,6 +29,45 @@ ListsRouterPsql.post('/lists', (req, res) => {
     if (err) { throw err; }
     res.json({'list_uuid': list_uuid});
     res.status(204);
+  });
+});
+
+/*
+* put /lists
+* @param list_uuid
+* @param list_name
+*/
+ListsRouterPsql.put('/lists', (req, res) => {
+  console.log('put /lists with ', req.query);
+  const {list_uuid, list_name, dec} = req.query;
+  if (list_name) {
+    postgres.query(putListsName, [list_name, list_uuid], (err, data) => {
+      if (err) { throw err; }
+      res.status(204);
+    });
+  } else if (dec) {
+    postgres.query(decListsTodoCount, [list_uuid], (err, data) => {
+      if (err) { throw err; }
+      res.status(204);
+    });
+  } else {
+      postgres.query(putListsTodoCount, [list_uuid], (err, data) => {
+        if (err) { throw err; }
+        res.status(204);
+      });
+  }
+});
+
+/*
+* delete /lists
+* @param list_uuid
+*/
+ListsRouterPsql.delete('/lists', (req, res) => {
+  const { list_uuid } = req.query
+  postgres.query(deleteLists, [list_uuid], (err, data) => {
+    if (err) { throw err; }
+    res.status(200);
+    res.json('Deleted list with uuid ', list_uuid);
   });
 });
 
