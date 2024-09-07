ecf8b622c374f209cdf60a98264c752a0e9c7445
Samuel Berston <samuelberston@Samuels-MacBook-Air.local>
2024-09-06 23:22:04 -0700

    remove unnecessary DROP statements

--- todo-app-db/schema.sql
+++ todo-app-db/schema.sql
@@ -1,12 +1,10 @@
-CREATE EXTENSION pgcrypto;
+--CREATE EXTENSION pgcrypto;
 
 --DROP DATABASE IF EXISTS todo;
 DROP SCHEMA IF EXISTS todo CASCADE;
 
 --CREATE DATABASE todo;
 CREATE SCHEMA todo;
-
-DROP TABLE IF EXISTS TODO.users CASCADE;
 
 CREATE TABLE todo.users (
   user_key SERIAL NOT NULL,
@@ -18,8 +16,6 @@ CREATE TABLE todo.users (
 
 INSERT INTO todo.users (user_uuid, user_id, user_name, user_email) VALUES ('c75321b1-084a-4a1f-9f7e-7e72bbd2ef13', 'google-oauth2|113421175681730408776', 'Samuel Berston', 'samuelberston@gmail.com');
 
-DROP TABLE IF EXISTS todo.lists CASCADE;
-
 CREATE TABLE todo.lists (
   list_key SERIAL NOT NULL,
   list_uuid UUID PRIMARY KEY,
@@ -29,9 +25,7 @@ CREATE TABLE todo.lists (
   FOREIGN KEY (user_uuid) REFERENCES todo.users(user_uuid)
 );
 
-INSERT INTO todo.lists (list_key, list_uuid, list_name, user_uuid) VALUES (1, 'f3d26195-4348-457a-a036-451cf05f7d22', E'Samuel\'s List', 'c75321b1-084a-4a1f-9f7e-7e72bbd2ef13');
-
-DROP TABLE IF EXISTS todo.todos CASCADE;
+INSERT INTO todo.lists (list_key, list_uuid, list_name, user_uuid) VALUES (1, 'f3d26195-4348-457a-a036-451cf05f7d22', E'Samuel''s List', 'c75321b1-084a-4a1f-9f7e-7e72bbd2ef13');
 
 CREATE TABLE todo.todos (
   todo_id SERIAL NOT NULL PRIMARY KEY,
@@ -56,8 +50,6 @@ VALUES ('0efccfd5-bea5-48ad-b29e-6d079525150a', 'Deploy the app on aws', 'We lov
 INSERT INTO todo.todos (todo_uuid, task, description, date_created, date_due, priority, user_uuid, list_uuid)
 VALUES ('d2396620-d623-44c8-b7a6-344654e671c1', 'Containerize the app with Docker', 'Eventually split up into containerized microservices', '2022-11-13T00:55:15.320Z', '2022-11-13T00:55:15.320Z', 'p4', 'c75321b1-084a-4a1f-9f7e-7e72bbd2ef13', 'f3d26195-4348-457a-a036-451cf05f7d22');
 
-DROP TABLE IF EXISTS TODO.tags CASCADE;
-
 CREATE TABLE todo.tags (
   tag_id SERIAL NOT NULL PRIMARY KEY,
   tag VARCHAR(25) NOT NULL
@@ -68,8 +60,6 @@ INSERT INTO todo.tags (tag) VALUES ('Personal');
 INSERT INTO todo.tags (tag) VALUES ('Social');
 INSERT INTO todo.tags (tag) VALUES ('Backlog');
 
-DROP TABLE IF EXISTS todo.todos_tags CASCADE;
-
 CREATE TABLE todo.todos_tags (
   todo_id INT NOT NULL,
   tag_id INT NOT NULL,
@@ -77,14 +67,13 @@ CREATE TABLE todo.todos_tags (
   FOREIGN KEY (tag_id) REFERENCES todo.tags(tag_id)
 );
 
+-- BUG: reference UUIDs instead of the autoincrement ID
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (1, 1);
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (2, 1);
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (3, 1);
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (3, 2);
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (3, 3);
 INSERT INTO todo.todos_tags (todo_id, tag_id) VALUES (3, 4);
-
-DROP TABLE IF EXISTS todo.subtasks CASCADE;
 
 CREATE TABLE todo.subtasks (
   subtask_key SERIAL NOT NULL,
