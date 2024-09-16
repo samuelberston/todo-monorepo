2d0a8f8822203940d6e227270ba9d5629dd9a194
Samuel Berston <samuelberston@gmail.com>
2024-09-16 01:00:09 -0700

    retrieve DB credentials from AWS secrests manager

--- todo-app-backend/server/psql.js
+++ todo-app-backend/server/psql.js
@@ -3,12 +3,48 @@ const { Client } = require('pg')
 const port = 5432;
 
 // for connection to docker postgresql (todo-db)
-//const user = 'postgres';
-const user = 'sberston';
-const password = 'docker';
+const user = 'postgres';
 //const host = 'host.docker.internal'; // for running todo-app with containerized db
 const host = '0.0.0.0'; // for running todo-app locally
 const database = 'todo';
+
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager({ region: 'us-east-1' }); // Adjust the region if necessary
+
+// get DB credentials from AWS Secrests Manager
+async function getDatabaseCredentials() {
+  try {
+    const data = await secretsManager.getSecretValue({ SecretId: 'rds-database-password' }).promise();
+
+    if (data.SecretString) {
+      const credentials = JSON.parse(data.SecretString);
+      return credentials;
+    } else {
+      throw new Error('Secret not found');
+    }
+  } catch (err) {
+    console.error('Error retrieving secret:', err);
+    throw err;
+  }
+}
+
+async function connectToDatabase() {
+  const credentials = await getDatabaseCredentials();
+
+  const client = new Client({
+    user: credentials.username,
+    host: 'threat-postgres-rds.cluster-ro-cwhep9aqborz.us-east-1.rds.amazonaws.com',
+    database: 'todo',
+    password: credentials.password,
+    port: 5432,
+  });
+
+  await client.connect();
+  console.log('Connected to PostgreSQL database');
+}
+
+connectToDatabase();
+
 
 const postgres = new Client({
     host,
