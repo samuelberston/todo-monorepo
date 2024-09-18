8e512295eb91eb7847b5828b4d779b3397216755
Samuel Berston <samuelberston@gmail.com>
2024-09-17 23:15:45 -0700

    UsersRouterPsql.js - validate email in /users POST request"

--- todo-app-backend/server/routes/UsersRouterPsql.js
+++ todo-app-backend/server/routes/UsersRouterPsql.js
@@ -20,14 +20,25 @@ UsersRouterPsql.get('/users', (req, res) => {
   });
 });
 
-UsersRouterPsql.post('/users', (req, res) => {
-  const user_uuid = uuidv4()
-  const user_id = req.auth.payload.sub;
-  const user_email = req.body.email;
-  postgres.query(postUser, [user_uuid, user_id, user_email], (err, data) => {
-    if (err) { throw err; }
-    res.status(201).send([user_id]);
-  });
+UsersRouterPsql.post(
+  '/users', 
+  body('email').isEmail().withMessage('Invalid email address'), 
+  (req, res) => {
+    // check for validation errors
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    
+    const user_uuid = uuidv4()
+    const user_id = req.auth.payload.sub;
+    const user_email = req.body.email;
+    postgres.query(postUser, [user_uuid, user_id, user_email], (err, data) => {
+      if (err) { 
+        res.status(500).json({ error: 'Database error' });
+      }
+      res.status(201).send([user_id]);
+    });
 });
 
 module.exports = UsersRouterPsql;
